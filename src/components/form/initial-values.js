75ad6b49e3f082ff316c65f8a5ae5bf7f9fe62a8
cohenrealestate1 <79107538+cohenrealestate1@users.noreply.github.com>
2021-02-17 20:03:38 -0500

    Barebones form with switch

--- src/components/form/initial-values.js
+++ src/components/form/initial-values.js
@@ -61,3 +61,5 @@ export const getInitialValues = (): FormValuesType => {
     },
   };
 };
+
+export default getInitialValues;
--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -1,9 +1,31 @@
 // @flow
 import React from "react";
 import Page from "../../components/page";
+import { withFormik, useFormikContext } from "formik";
+import getInitialValues from "../../components/form/initial-values";
+import Step1 from "./step-1";
+import Step2 from "./step-2";
 
 export const Form = () => {
-  return <Page content={"This is a form"} />;
+  const {
+    values: { step },
+  } = useFormikContext();
+  const getContent = () => {
+    switch (step) {
+      case 1:
+        return <Step1 />;
+      case 2:
+      default:
+        return <Step2 />;
+    }
+  };
+  return <Page content={getContent()} />;
 };
 
-export default Form;
+export default withFormik({
+  validateOnBlur: false,
+  validateOnChange: false,
+  mapPropsToValues: getInitialValues,
+  handleSubmit: () => {},
+  validate: () => {},
+})(Form);
