512971bb83ed8f569e10f50c3e6b68bb582b1e5e
cohenrealestate1 <79107538+cohenrealestate1@users.noreply.github.com>
2021-02-23 22:38:22 -0500

    Fix select code

--- src/components/shared/select.js
+++ src/components/shared/select.js
@@ -1,23 +1,6 @@
 // @flow
 import React from "react";
-import {
-  Select as MaterialSelect,
-  MenuItem,
-  TextField,
-} from "@material-ui/core";
-import { useFormikContext } from "formik";
-import { StyledFormControlLabel } from "./styled-components";
-import styled from "styled-components";
 import TextField2 from "./text-field";
-
-const StyledMaterialSelect = styled(MaterialSelect)`
-  &.MuiInputBase-root {
-    background-color: white;
-  }
-  &.MuiInput-underline:before {
-    border-bottom: unset;
-  }
-`;
 
 interface Props {
   fieldName: string;
@@ -27,18 +10,7 @@ interface Props {
 }
 
 export const Select = (props: Props) => {
-  const { setFieldValue } = useFormikContext();
-  const { fieldName, label, value, options } = props;
-  return (
-    <TextField2
-      label={label}
-      select={true}
-      value={value}
-      variant="outlined"
-      options={options}
-      onChange={(event) => setFieldValue(fieldName, event.target.value, false)}
-    />
-  );
+  return <TextField2 select={true} variant="outlined" {...props} />;
 };
 
 export default Select;
