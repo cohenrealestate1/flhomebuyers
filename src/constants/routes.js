9c4cf6d45d6970b4d203100ca173f28ddf6d0158
bencohen3 <77013803+bencohen3@users.noreply.github.com>
2021-04-02 18:01:38 -0400

    Routes

---
+++ src/constants/routes.js
@@ -0,0 +1,12 @@
+// @flow
+export const Routes = {
+    Home: "/",
+    ContactUs: "/contact-us",
+    Form: "/get-cash-offer",
+    Covid19: "/covid-19",
+    AboutUs: "/about-us",
+    Testimonials: "/testimonials",
+    Blog: "/blog",
+    HowItWorks: "/how-it-works",
+  };
+  
--- src/root.jsx
+++ src/root.jsx
@@ -2,15 +2,19 @@
 import React from "react";
 import { Route, Switch } from "fusion-plugin-react-router";
 import styled from "styled-components";
+import {Routes} from "./constants";
 
 const Container = styled.div`
   margin: -8px;
 `;
 
+const unimplemented = () => "Hello world";
+
 const root = (
   <Container>
     <Switch>
-      <Route exact path="/" component={() => <div>A</div>} />
+    <Route path={[Routes.ContactUs, Routes.Form, Routes.Covid19, Routes.AboutUs, Routes.Testimonials, Routes.Blog, Routes.HowItWorks]} component={unimplemented} />
+    <Route exact path={Routes.Home} component={() => "A"} />
     </Switch>
   </Container>
 );
