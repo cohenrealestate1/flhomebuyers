71a6bbda5d7d5c22bedbfcdd9cb05fc9206a25b8
bencohen3 <77013803+bencohen3@users.noreply.github.com>
2021-04-12 18:38:09 -0400

    Remove OLD and unused dependencies

--- src/components/shared/styled-components.js
+++ src/components/shared/styled-components.js
@@ -1,22 +1,6 @@
 // @flow
-import { FormControlLabel } from "@material-ui/core";
 import styled from "styled-components";
 import { Colors, Typographies } from "../../constants";
-
-export const StyledFormControlLabel = styled(FormControlLabel)`
-  &.MuiFormControlLabel-root {
-    display: flex;
-    flex-direction: column;
-    flex-direction: column-reverse;
-    align-items: flex-start;
-    margin-left: 0px;
-    margin-right: 0px;
-    width: 100%;
-  }
-  & .MuiFormControlLabel-label {
-    font-weight: 700;
-  }
-`;
 
 export const StyledAdornment = styled.img`
   height: 40px;
