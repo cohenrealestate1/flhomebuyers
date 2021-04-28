5782455058466e8c29b3482cf3337163efe36741
bencohen3 <77013803+bencohen3@users.noreply.github.com>
2021-04-27 22:36:47 -0400

    Fix blog redering

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -22,7 +22,6 @@ const BlogSection = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    gap: 53px;
 `;
 const CallFormContainer = styled.div`
     padding-top: 30px;
--- src/components/shared/blog-card.js
+++ src/components/shared/blog-card.js
@@ -1,9 +1,14 @@
 // @flow
 import React from "react";
+import styled from "styled-components";
 import { Colors, makeMediaQueryMax, Typographies } from "../../constants";
 import { BlogType } from "../../types";
 import Text from "../text";
 import { CardContainer, CardImg, CardRightContainer } from "./styled-components";
+
+const Container = styled.div`
+    margin-bottom: 53px;
+`
 
 type Props = {
     blog: BlogType,
@@ -12,7 +17,7 @@ type Props = {
 
 export const BlogCard = (props: Props) => {
     const {blog: {imgHref, quote, title, url}, color} = props;
-    return <CardContainer>
+    return <Container><CardContainer>
         <CardImg src={imgHref} />
         <CardRightContainer $color={color}>
             <Text $color={Colors.White} $margin="0 0 40px 0" $uppercase $fontWeight={700} $typography={Typographies.SubHeaderSmall} $style={makeMediaQueryMax(1000, Typographies.SubHeaderMedium)} $openSans>{title}</Text>
@@ -20,6 +25,7 @@ export const BlogCard = (props: Props) => {
             <Text $color={Colors.White} $lineHeight="40px" $a $href={url} $underline $fontWeight="normal" $typography={Typographies.TextSmall} $openSans>Read Post</Text>
         </CardRightContainer>
     </CardContainer>
+    </Container>
 }
 
 export default BlogCard;
