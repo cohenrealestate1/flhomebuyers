16149140086c3330fedab56dbb00b7935a5174c1
bencohen3 <77013803+bencohen3@users.noreply.github.com>
2021-05-09 22:11:32 -0400

    Dad contractor number

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,4 +3,4 @@ export const SiteName = "FL Home Cash Buyers";
 export const HouseImageHref = 'https://infinitepest.com/wp-content/uploads/2021/03/green-grass-in-the-front-yard-of-a-florida-home-KLEDP5E-scaled.jpg';
 export const TestimonialImageHref = 'https://lh3.googleusercontent.com/z3cbBAyRvshEdr4cfzj3SIKMAbuOh8o0yW5RWA6zd76jredlNQObtHgolx8kBDo27JoQ7w=s400';
 export const Covid19Href = 'https://shepherdaz.church/wp-content/uploads/2021/02/masked-resized.jpg';
-export const DadContractorNumber = "123";
+export const DadContractorNumber = "CRC1333018";
